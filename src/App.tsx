0ff6b6265cc4fb646ef4868972234fec371ed02c
yechu <yechukim217@gmail.com>
2022-08-13 17:43:25 +0900

    feat: add router and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import Layout from './components/Layout'
+import { Route, Routes } from 'react-router-dom'
+import SideMenu from './components/SideMenu'
+import BookmarkPage from './pages/BookmarkPage'
+import HomePage from './pages/HomePage'
 import './styles/main.scss'
 
 function App() {
 	return (
-		<Layout>
-			<div></div>
-		</Layout>
+		<>
+			<SideMenu />
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/bookmarks" element={<BookmarkPage />} />
+			</Routes>
+		</>
 	)
 }
 
---
+++ src/pages/BookmarkPage.tsx
@@ -0,0 +1,5 @@
+function BookmarkPage() {
+	return <div>bookmark</div>
+}
+
+export default BookmarkPage
