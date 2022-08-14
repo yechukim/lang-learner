b1568687b00d222043e1cdfa2bcc588300a2d518
yechu <yechukim217@gmail.com>
2022-08-14 19:51:13 +0900

    feat: add WriteModal

---
+++ src/components/WriteModal.tsx
@@ -0,0 +1,43 @@
+import classNames from 'classnames'
+import { useContext } from 'react'
+import { ThemeContext } from '../context/ThemeContext'
+import languages from '../data/languages'
+import './WriteModal.scss'
+
+function WriteModal() {
+	const { theme } = useContext(ThemeContext)
+	return (
+		<div className={classNames('ModalWrapper', theme)}>
+			<div className="Modal">
+				<h2>What did you learn today?</h2>
+				<form action="">
+					<LanguageSelect target />
+					<LanguageSelect />
+				</form>
+			</div>
+		</div>
+	)
+}
+
+export default WriteModal
+
+type LangType = {
+	target?: boolean
+}
+
+function LanguageSelect({ target }: LangType) {
+	return (
+		<>
+			<div className={classNames('Selection', target)}>
+				<label htmlFor={'lang' + target} />
+				<select name="language" id={'lang' + target}>
+					<option value="">languages</option>
+					{languages.map((item) => (
+						<option value={item.name}>{item.name}</option>
+					))}
+				</select>
+			</div>
+			<textarea className="TargetArea" name="Text1" cols={40} rows={5} />
+		</>
+	)
+}
