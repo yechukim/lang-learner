ac04be8fff921a79d18bb37b5597dab236839ab3
yechu <yechukim217@gmail.com>
2022-08-15 14:54:16 +0900

    feat: add TextButton

---
+++ src/components/TextButton.tsx
@@ -0,0 +1,26 @@
+import classNames from 'classnames'
+import React from 'react'
+import './TextButton.scss'
+
+type ButtonType = {
+	text: String
+	handleClick: () => void
+	size: 'small' | 'large'
+	color: 'pink' | 'yellow' | 'orange' | 'green' | 'blue'
+}
+
+function TextButton({ text, handleClick, size, color }: ButtonType) {
+	const handleButton = (e: React.MouseEvent<HTMLElement>) => {
+		e.preventDefault()
+		handleClick()
+	}
+	return (
+		<div className="TextButton">
+			<button className={classNames(size, color)} onClick={handleButton}>
+				{text}
+			</button>
+		</div>
+	)
+}
+
+export default TextButton
