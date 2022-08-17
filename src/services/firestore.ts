061b17953c1ff6d393c3e65648483418f4b2652e
yechu <yechukim217@gmail.com>
2022-08-17 18:54:30 +0900

    feat: add .env

--- src/services/firestore.ts
+++ src/services/firestore.ts
@@ -1,21 +1,44 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
+import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const {
+	VITE_FIREBASE_API_KEY,
+	VITE_FIREBASE_AUTH_DOMAIN,
+	VITE_FIREBASE_PROJECT_ID,
+	VITE_FIREBASE_STORAGE_BUCKET,
+	VITE_FIREBASE_SENDER_ID,
+	VITE_FIREBASE_APP_ID,
+	VITE_FIREBASE_MEASUREMENT_ID,
+} = import.meta.env
+console.log(import.meta.env.VITE_FIREBASE_API_KEY)
+// console.log(
+// 	VITE_FIREBASE_API_KEY,
+// 	VITE_FIREBASE_AUTH_DOMAIN,
+// 	VITE_FIREBASE_PROJECT_ID,
+// 	VITE_FIREBASE_STORAGE_BUCKET,
+// 	VITE_FIREBASE_SENDER_ID,
+// 	VITE_FIREBASE_APP_ID,
+// 	VITE_FIREBASE_MEASUREMENT_ID
+// )
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: '216126351230',
-	appId: '1:216126351230:web:2db667f6a01a222c0f752f',
-	measurementId: 'G-LWQMR8XZMG',
+	apiKey: VITE_FIREBASE_API_KEY,
+	authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: VITE_FIREBASE_PROJECT_ID,
+	storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: VITE_FIREBASE_SENDER_ID,
+	appId: VITE_FIREBASE_APP_ID,
+	measurementId: VITE_FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
+//const analytics = getAnalytics(app)
+const db = getFirestore(app)
+export { db }
